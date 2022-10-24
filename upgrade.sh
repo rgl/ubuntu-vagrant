54ec5919bac37e607e3d7a4a759fd641e7a44976
Rui Lopes <rgl@ruilopes.com>
2022-10-24 16:44:21 +0100

    disable unattended upgrades

--- upgrade.sh
+++ upgrade.sh
@@ -11,6 +11,12 @@ sed -i -E 's,^%sudo\s+.+,%sudo ALL=(ALL) NOPASSWD:ALL,g' /etc/sudoers
 # configure apt for non-interactive mode.
 export DEBIAN_FRONTEND=noninteractive
 
+# disable unattended upgrades.
+# NB it interferes with our automation with errors alike:
+#       E: Could not get lock /var/lib/dpkg/lock-frontend. It is held by process 17599 (unattended-upgr)
+#       E: Unable to acquire the dpkg frontend lock (/var/lib/dpkg/lock-frontend), is another process using it?
+apt-get remove -y --purge unattended-upgrades
+
 # upgrade.
 apt-get update
 apt-get dist-upgrade -y
